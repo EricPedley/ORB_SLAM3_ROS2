5213cc50222b52639b38db2cd4e5f2ee0efa8935
Graham Clifford <gjcliff@gmail.com>
2024-10-13 19:58:06 -0500

    Can visualize point clouds in rviz now
    Looks like the issues I was having with the orientations being messed up
    is fixed when VIBA 2 completes I think, maybe even with just VIBA 1.
    Point cloud is still flipped vertically which I will now attempt to fix.

--- src/visualize_point_cloud.cpp
+++ src/visualize_point_cloud.cpp
@@ -1,7 +1,12 @@
 #include <chrono>
 #include <functional>
 #include <memory>
+#include <pcl/PCLPointCloud2.h>
 #include <pcl/filters/statistical_outlier_removal.h>
+#include <pcl/impl/point_types.hpp>
+#include <pcl/point_cloud.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <rclcpp/logging.hpp>
 #include <sensor_msgs/msg/detail/point_field__struct.hpp>
 #include <string>
 #include <fstream>
@@ -15,9 +20,7 @@
 #include <geometry_msgs/msg/point32.hpp>
 #include <geometry_msgs/msg/quaternion.hpp>
 
-#include <pcl_conversions/pcl_conversions.h>
-#include <pcl/point_cloud.h>
-#include <pcl/point_types.h>
+#include <pcl/io/pcd_io.h>
 
 #include "tf2/LinearMath/Quaternion.h"
 #include "tf2_ros/transform_broadcaster.h"
@@ -25,7 +28,7 @@
 #include <nav_msgs/msg/occupancy_grid.hpp>
 
 using namespace std::chrono_literals;
-using std::placeholders::_1;
+// using std::placeholders::_1;
 
 class VisualizePointCloud : public rclcpp::Node
 {
@@ -35,16 +38,14 @@ class VisualizePointCloud : public rclcpp::Node
     {
       // declare parameters
       declare_parameter("map_file_name", "2024-05-05_01:44:37 PM_graham_apt_imu_map.csv");
+      declare_parameter("pcl_file", "changeme.pcd");
       std::string file_name = get_parameter("map_file_name").as_string();
+      pcl_file = get_parameter("pcl_file").as_string();
 
       map_file_path = static_cast<std::string>(PROJECT_PATH) + "/maps/"+ file_name;
 
-      // create subscribers
-      // occupancy_grid_sub = create_subscription<nav_msgs::msg::OccupancyGrid>(
-      //     "/projected_map", 10, std::bind(&VisualizePointCloud::occupancy_grid_callback, this, _1));
-
       // create publishers
-      point_cloud_publisher = create_publisher<sensor_msgs::msg::PointCloud>("orb_point_cloud", 10);
+      // point_cloud_publisher = create_publisher<sensor_msgs::msg::PointCloud>("orb_point_cloud", 10);
 
       point_cloud2_publisher = create_publisher<sensor_msgs::msg::PointCloud2>("orb_point_cloud2", 10);
 
@@ -59,157 +60,42 @@ class VisualizePointCloud : public rclcpp::Node
     }
 
   private:
-    // void occupancy_grid_callback(const nav_msgs::msg::OccupancyGrid msg)
-    // {
-    //
-    // }
     void load_point_cloud()
     {
+      RCLCPP_INFO_STREAM(get_logger(), "loading point cloud from " << std::string(PROJECT_PATH) + "/maps/" + pcl_file);
+      pcl::PointCloud<pcl::PointXYZ> pcl_cloud;
+      pcl::io::loadPCDFile(std::string(PROJECT_PATH) + "/maps/" + pcl_file, pcl_cloud);
 
-      std::ifstream file(map_file_path);
-      if (file.fail())
-      {
-        RCLCPP_ERROR_STREAM(get_logger(), "Failed to open file: " << map_file_path);
-        rclcpp::shutdown();
-      }
-      std::string line;
-      int line_num = 0;
-      while (std::getline(file, line))
-      {
-        std::istringstream iss(line);
-        std::string token;
-        int i = 0;
-        if (line_num == 0)
-        {
-          geometry_msgs::msg::Quaternion orientation;
-          while (std::getline(iss, token, ','))
-          {
-            if (i == 0)
-            {
-              orientation.x = std::stof(token);
-            }
-            else if (i == 1)
-            {
-              orientation.y = std::stof(token);
-            }
-            else if (i == 2)
-            {
-              orientation.z = std::stof(token);
-            }
-            else if (i == 3)
-            {
-              orientation.w = std::stof(token);
-            }
-            i++;
-          }
-          line_num++;
-          initial_orientation = tf2::Quaternion(orientation.x, orientation.y, orientation.z, orientation.w);
-          continue;
-        }
-        geometry_msgs::msg::Point32 point;
-        while (std::getline(iss, token, ','))
-        {
-          if (i == 0)
-          {
-            point.x = std::stof(token);
-          }
-          else if (i == 1)
-          {
-            point.y = std::stof(token);
-          }
-          else if (i == 2)
-          {
-            point.z = std::stof(token);
-          }
-          i++;
-        }
-        point_cloud.points.push_back(point);
-      }
-
-      point_cloud.header.frame_id = "point_cloud";
-      point_cloud.header.stamp = get_clock()->now();
-      point_cloud.channels.push_back(sensor_msgs::msg::ChannelFloat32());
-      point_cloud.channels[0].name = "intensity";
-      point_cloud.channels[0].values.resize(point_cloud.points.size());
-
-      for (int i = 0; i < point_cloud.points.size(); i++)
-      {
-        point_cloud.channels[0].values[i] = 1.0;
-      }
-      RCLCPP_INFO_STREAM(get_logger(), "Loaded point cloud with " << point_cloud.points.size() << " points");
-      sensor_msgs::msg::PointField x_field;
-      x_field.name = "x";
-      x_field.offset = 0;
-      x_field.datatype = sensor_msgs::msg::PointField::FLOAT32;
-      x_field.count = 1;
-
-      sensor_msgs::msg::PointField y_field;
-      y_field.name = "y";
-      y_field.offset = 4;
-      y_field.datatype = sensor_msgs::msg::PointField::FLOAT32;
-      y_field.count = 1;
-
-      sensor_msgs::msg::PointField z_field;
-      z_field.name = "z";
-      z_field.offset = 8;
-      z_field.datatype = sensor_msgs::msg::PointField::FLOAT32;
-      z_field.count = 1;
-
-      sensor_msgs::msg::PointField intensity_field;
-      intensity_field.name = "intensity";
-      intensity_field.offset = 12;
-      intensity_field.datatype = sensor_msgs::msg::PointField::FLOAT32;
-      intensity_field.count = 1;
-
-      point_cloud2.header.frame_id = "point_cloud";
-      point_cloud2.header.stamp = get_clock()->now();
-      point_cloud2.fields.push_back(x_field);
-      point_cloud2.fields.push_back(y_field);
-      point_cloud2.fields.push_back(z_field);
-
-      point_cloud2.height = 1;
-      point_cloud2.width = point_cloud.points.size();
-      point_cloud2.point_step = 12;
-      point_cloud2.row_step = 12 * point_cloud.points.size();
-      point_cloud2.is_dense = true;
-      point_cloud2.is_bigendian = false;
-      point_cloud2.data.resize(point_cloud2.row_step);
-      for (int i = 0; i < point_cloud.points.size(); i++)
-      {
-        memcpy(&point_cloud2.data[i * 12], &point_cloud.points[i].x, 4);
-        memcpy(&point_cloud2.data[i * 12 + 4], &point_cloud.points[i].y, 4);
-        memcpy(&point_cloud2.data[i * 12 + 8], &point_cloud.points[i].z, 4);
-      }
+      RCLCPP_INFO_STREAM(get_logger(), "loaded " << pcl_cloud.points.size() << " points");
 
       // now i can filter the pointcloud
-      pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
-      pcl::fromROSMsg(point_cloud2, *cloud);
+      pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_ptr = std::make_shared<pcl::PointCloud<pcl::PointXYZ>>(pcl_cloud);
       pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZ>);
       pcl::StatisticalOutlierRemoval<pcl::PointXYZ> sor;
-      sor.setInputCloud(cloud);
+      sor.setInputCloud(cloud_ptr);
       sor.setMeanK(75);
       sor.setStddevMulThresh(1.0);
       sor.filter(*cloud_filtered);
       pcl::toROSMsg(*cloud_filtered, point_cloud2);
-
+      point_cloud2.header.frame_id = "map";
     }
     void timer_callback()
     {
-      geometry_msgs::msg::TransformStamped t;
-      t.header.stamp = get_clock()->now();
-      t.header.frame_id = "map";
-      t.child_frame_id = "point_cloud";
+      // geometry_msgs::msg::TransformStamped t;
+      // t.header.stamp = get_clock()->now();
+      // t.header.frame_id = "map";
+      // t.child_frame_id = "point_cloud";
+      //
+      // t.transform.rotation.x = initial_orientation.x();
+      // t.transform.rotation.y = initial_orientation.y();
+      // t.transform.rotation.z = initial_orientation.z();
+      // t.transform.rotation.w = initial_orientation.w();
+      // t.transform.translation.z = 0.5;
+      // tf_broadcaster->sendTransform(t);
 
-      t.transform.rotation.x = initial_orientation.x();
-      t.transform.rotation.y = initial_orientation.y();
-      t.transform.rotation.z = initial_orientation.z();
-      t.transform.rotation.w = initial_orientation.w();
-      t.transform.translation.z = 0.5;
-      tf_broadcaster->sendTransform(t);
-
-      point_cloud.header.stamp = get_clock()->now();
+      // point_cloud.header.stamp = get_clock()->now();
       point_cloud2.header.stamp = get_clock()->now();
-      point_cloud_publisher->publish(point_cloud);
+      // point_cloud_publisher->publish(point_cloud);
       point_cloud2_publisher->publish(point_cloud2);
     }
     rclcpp::TimerBase::SharedPtr timer;
@@ -221,6 +107,7 @@ class VisualizePointCloud : public rclcpp::Node
     tf2::Quaternion initial_orientation;
     std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster;
     std::string map_file_path;
+    std::string pcl_file;
 
 };
 
