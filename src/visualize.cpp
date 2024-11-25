4b8ab505c25f67ae57b98c3352bdf962ab7e49bb
Graham Clifford <gjcliff@gmail.com>
2024-11-24 22:00:16 -0600

    Added node for visualizing semanticly labeled objects

--- src/visualize.cpp
+++ src/visualize.cpp
@@ -4,6 +4,7 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include <filesystem>
+#include <pcl_conversions/pcl_conversions.h>
 #include <sensor_msgs/msg/point_cloud2.hpp>
 
 using namespace std::chrono_literals;
@@ -14,27 +15,33 @@ public:
   {
     // declare parameters
     declare_parameter("objects_path", "");
+    declare_parameter("cloud_path", "");
 
     // get parameters
     get_parameter("objects_path", objects_path_);
+    get_parameter("cloud_path", cloud_path_);
 
     if (!load_clouds()) {
       RCLCPP_ERROR(get_logger(), "Error loading clouds");
       rclcpp::shutdown();
     }
+
+    combine_clouds();
   }
 
 private:
   bool load_clouds()
   {
-    std::string path =
-      std::string(PROJECT_PATH) + "/semantic_maps/" + objects_path_;
-    if (!std::filesystem::is_directory(path)) {
+    std::string object_path =
+      std::string(PROJECT_PATH) + "/objects/" + objects_path_;
+    std::string cloud_path =
+      std::string(PROJECT_PATH) + "/clouds/" + cloud_path_;
+    if (!std::filesystem::is_directory(object_path)) {
       RCLCPP_ERROR_STREAM(get_logger(),
-                          "Directory " << path << "does not exist");
-      return false;
+                          "Directory " << object_path << "does not exist"); return false;
     } else {
-      for (const auto &file : std::filesystem::directory_iterator(path)) {
+      for (const auto &file :
+           std::filesystem::directory_iterator(object_path)) {
         if (file.is_regular_file() && file.path().extension() == ".pcd") {
           pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(
             new pcl::PointCloud<pcl::PointXYZRGB>);
@@ -47,14 +54,51 @@ private:
         }
       }
     }
+    if (!std::filesystem::is_directory(cloud_path)) {
+      RCLCPP_ERROR_STREAM(get_logger(),
+                          "Directory " << cloud_path << "does not exist");
+      return false;
+    } else {
+      if (pcl::io::loadPCDFile(cloud_path, full_cloud_) == -1) {
+        RCLCPP_ERROR_STREAM(get_logger(), "Error loading file " << cloud_path);
+        return false;
+      }
+    }
     return true;
+  }
+
+  void combine_clouds()
+  {
+    for (const auto &cloud : clouds_) {
+      combined_cloud_ += *cloud;
+    }
   }
   rclcpp::TimerBase::SharedPtr timer_;
   rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr
     point_cloud_publisher_;
 
+  void timer_callback()
+  {
+    sensor_msgs::msg::PointCloud2 cloud_msg;
+    pcl::toROSMsg(combined_cloud_, cloud_msg);
+    cloud_msg.header.frame_id = "world";
+    cloud_msg.header.stamp = get_clock()->now();
+    point_cloud_publisher_->publish(cloud_msg);
+
+    for (const auto &cloud : clouds_) {
+      sensor_msgs::msg::PointCloud2 cloud_msg;
+      pcl::toROSMsg(*cloud, cloud_msg);
+      cloud_msg.header.frame_id = "world";
+      cloud_msg.header.stamp = get_clock()->now();
+      point_cloud_publisher_->publish(cloud_msg);
+    }
+  }
+
   std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr> clouds_;
+  pcl::PointCloud<pcl::PointXYZRGB> combined_cloud_;
+  pcl::PointCloud<pcl::PointXYZRGB> full_cloud_;
   std::string objects_path_;
+  std::string cloud_path_;
 };
 
 int main(int argc, char *argv[])
