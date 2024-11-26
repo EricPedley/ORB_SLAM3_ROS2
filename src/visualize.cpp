24c74f0b9b8d73b6daf114af61565cb09c89afa9
Graham Clifford <gjcliff@gmail.com>
2024-11-26 15:51:21 -0600

    possibly added bounding boxes to the visualization

--- src/visualize.cpp
+++ src/visualize.cpp
@@ -1,13 +1,27 @@
 #include <pcl/impl/point_types.hpp>
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_cloud.h>
+#include <pcl_conversions/pcl_conversions.h>
+
+#include <sensor_msgs/msg/point_cloud2.hpp>
+#include <visualization_msgs/msg/detail/marker_array__struct.hpp>
+#include <visualization_msgs/msg/marker_array.hpp>
+#include <visualization_msgs/msg/marker.hpp>
+
+#include <filesystem>
 
 #include "rclcpp/rclcpp.hpp"
-#include <filesystem>
-#include <pcl_conversions/pcl_conversions.h>
-#include <sensor_msgs/msg/point_cloud2.hpp>
 
 using namespace std::chrono_literals;
+
+struct Box {
+  float x_min = std::numeric_limits<float>::infinity();
+  float x_max = -std::numeric_limits<float>::infinity();
+  float y_min = std::numeric_limits<float>::infinity();
+  float y_max = -std::numeric_limits<float>::infinity();
+  float z_min = std::numeric_limits<float>::infinity();
+  float z_max = -std::numeric_limits<float>::infinity();
+};
 
 class Visualize : public rclcpp::Node {
 public:
@@ -94,8 +108,74 @@ private:
   {
     for (const auto &cloud : clouds_) {
       combined_cloud_ += *cloud;
+      bounding_boxes_.push_back(calculate_box(*cloud));
     }
   }
+
+  std::vector<geometry_msgs::msg::Point> calculate_box(pcl::PointCloud<pcl::PointXYZRGB> &cloud)
+  {
+    Box box;
+    for (const auto &point : cloud) {
+      box.x_max = point.x > box.x_max ? point.x : box.x_max;
+      box.x_min = point.x < box.x_min ? point.x : box.x_min;
+      box.y_max = point.y > box.y_max ? point.y : box.y_max;
+      box.y_min = point.y < box.y_min ? point.y : box.y_min;
+      box.z_max = point.z > box.z_max ? point.z : box.z_max;
+      box.z_min = point.z < box.z_min ? point.z : box.z_min;
+    }
+
+    std::vector<geometry_msgs::msg::Point> points;
+    geometry_msgs::msg::Point top_right_front;
+    top_right_front.x = box.x_max;
+    top_right_front.y = box.y_min;
+    top_right_front.z = box.z_max;
+    points.push_back(top_right_front);
+
+    geometry_msgs::msg::Point top_right_back;
+    top_right_back.x = box.x_max;
+    top_right_back.y = box.y_max;
+    top_right_back.z = box.z_max;
+    points.push_back(top_right_back);
+
+    geometry_msgs::msg::Point top_left_back;
+    top_left_back.x = box.x_min;
+    top_left_back.y = box.y_max;
+    top_left_back.z = box.z_max;
+    points.push_back(top_left_back);
+
+    geometry_msgs::msg::Point top_left_front;
+    top_left_front.x = box.x_min;
+    top_left_front.y = box.y_min;
+    top_left_front.z = box.z_max;
+    points.push_back(top_left_front);
+
+    geometry_msgs::msg::Point bottom_left_front;
+    bottom_left_front.x = box.x_min;
+    bottom_left_front.y = box.y_min;
+    bottom_left_front.z = box.z_min;
+    points.push_back(bottom_left_front);
+
+    geometry_msgs::msg::Point bottom_right_front;
+    bottom_right_front.x = box.x_max;
+    bottom_right_front.y = box.y_min;
+    bottom_right_front.z = box.z_min;
+    points.push_back(bottom_right_front);
+
+    geometry_msgs::msg::Point bottom_right_back;
+    bottom_right_back.x = box.x_max;
+    bottom_right_back.y = box.y_max;
+    bottom_right_back.z = box.z_min;
+    points.push_back(bottom_right_back);
+
+    geometry_msgs::msg::Point bottom_left_back;
+    bottom_left_back.x = box.x_min;
+    bottom_left_back.y = box.y_max;
+    bottom_left_back.z = box.z_min;
+    points.push_back(bottom_left_back);
+
+    return points;
+  }
+
   void timer_callback()
   {
     sensor_msgs::msg::PointCloud2 full_cloud_msg;
@@ -110,6 +190,17 @@ private:
     object_cloud_msg.header.stamp = get_clock()->now();
     object_cloud_publisher_->publish(object_cloud_msg);
 
+    visualization_msgs::msg::MarkerArray box_markers;
+    for (size_t i = 0; i < bounding_boxes_.size(); i++) {
+      visualization_msgs::msg::Marker box_marker;
+      box_marker.header.frame_id = "map";
+      box_marker.header.stamp = get_clock()->now();
+      box_marker.id = i;
+      box_marker.action = visualization_msgs::msg::Marker::ADD;
+      box_marker.points = bounding_boxes_.at(i);
+      box_marker.type = visualization_msgs::msg::Marker::LINE_STRIP;
+    }
+
     iterator++;
     iterator = iterator == clouds_.size() ? 0 : iterator;
   }
@@ -121,6 +212,7 @@ private:
     object_cloud_publisher_;
 
   std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr> clouds_;
+  std::vector<std::vector<geometry_msgs::msg::Point>> bounding_boxes_;
   pcl::PointCloud<pcl::PointXYZRGB> combined_cloud_;
   pcl::PointCloud<pcl::PointXYZRGB> full_cloud_;
   std::string database_output_name_;
