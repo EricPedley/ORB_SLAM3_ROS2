2df00980227145c7186faf3072a1d67085309d67
Eric Pedley <ericpedley@gmail.com>
2025-08-05 14:19:37 -0700

    make cam noed undistort image

--- cam_node.py
+++ cam_node.py
@@ -3,10 +3,11 @@
 import rclpy
 from rclpy.node import Node
 from rclpy.qos import QoSProfile, ReliabilityPolicy, DurabilityPolicy
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, CameraInfo
 from cv_bridge import CvBridge
 import cv2
 from erics_cameras import RTSPCamera
+import numpy as np
 
 
 class CameraPublisher(Node):
@@ -19,16 +20,37 @@ class CameraPublisher(Node):
         image_qos.durability = DurabilityPolicy.TRANSIENT_LOCAL
         
         # Create publisher with QoS profile
-        self.publisher = self.create_publisher(Image, 'camera/image_raw', image_qos)
+        self.publisher = self.create_publisher(Image, '/image_rect', image_qos)
+        self.info_publisher = self.create_publisher(CameraInfo, '/camera_info_rect', image_qos)
         
         # Create cv_bridge instance
         self.bridge = CvBridge()
         
         self.cam = RTSPCamera()
+
+        self.cam_mat = np.array([[1028,0,609.03],[0,1028.4,397.99],[0,0,1]])
+        self.dist_coeffs = np.array([[-0.35952,0.080321,0.001794,-0.001439,0.025185]])
+
+        self.undistorted_cam_mat, _ = cv2.getOptimalNewCameraMatrix(self.cam_mat, self.dist_coeffs, (1920, 1080), 0, (1920, 1080))
+        self.m1, self.m2 = cv2.initUndistortRectifyMap(self.cam_mat, self.dist_coeffs, None, self.undistorted_cam_mat, (1920, 1080), cv2.CV_16SC2)
         
         # Create timer for publishing at ~30 FPS
         timer_period = 1.0 / 30.0  # 30 FPS
         self.timer = self.create_timer(timer_period, self.timer_callback)
+
+        info_timer_period = 1.0
+        self.info_timer = self.create_timer(info_timer_period, self.info_timer_callback)
+
+        self.camera_info = CameraInfo()
+        self.camera_info.header.stamp = self.get_clock().now().to_msg()
+        self.camera_info.header.frame_id = 'map'
+        self.camera_info.height = 1080
+        self.camera_info.width = 1920
+        self.camera_info.distortion_model = "pinhole"
+        self.camera_info.k = [1028.0, 0.0, 609.03, 0.0, 1028.4, 397.99, 0.0, 0.0, 1.0]
+        self.camera_info.d = []
+        self.camera_info.r = [1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0]
+        self.camera_info.p = [1028.0, 0.0, 609.03, 0.0, 0.0, 1028.4, 397.99, 0.0, 0.0, 0.0, 1.0, 0.0]
         
         self.get_logger().info('Camera publisher node started')
     
@@ -40,10 +62,12 @@ class CameraPublisher(Node):
             try:
                 # Convert OpenCV image to ROS Image message
                 frame = img.get_array()
-                gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                # gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                # ros_image = self.bridge.cv2_to_imgmsg(cv2.resize(gray_frame, (640, 480)), encoding='mono8')
+                frame_undistorted = cv2.remap(frame, self.m1, self.m2, cv2.INTER_LINEAR)
                 
                 # Convert OpenCV image to ROS Image message
-                ros_image = self.bridge.cv2_to_imgmsg(cv2.resize(gray_frame, (640, 480)), encoding='mono8')
+                ros_image = self.bridge.cv2_to_imgmsg(frame_undistorted, encoding='8UC3')
                 
                 # Set timestamp
                 ros_image.header.stamp = self.get_clock().now().to_msg()
@@ -57,6 +81,9 @@ class CameraPublisher(Node):
         else:
             self.get_logger().warn('Failed to capture frame')
     
+    def info_timer_callback(self):
+        self.info_publisher.publish(self.camera_info)
+
     def destroy_node(self):
         # Clean up resources
         if hasattr(self, 'cap') and self.cap.isOpened():
